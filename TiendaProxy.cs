bec0e734d53bf766f6b5f803b0558cab58d1a6de
jenniferve09 <105527570+jenniferve09@users.noreply.github.com>
2022-07-31 17:26:01 -0500

    Add files via upload

---
+++ TiendaProxy.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace prueba4444
+{
+    public class TiendaProxy
+    {
+        //Creamos la interfaz con su metodo de petición
+        public interface Contras
+        {
+            void Peticion(int pOpcion);
+        }
+        public class Proxycontras : Contras
+        {
+            private Contraseñas nombre;
+
+            //Implementación del metodo "Petición" para llamar a nuestros procesos
+            public void Peticion(int pOpcion)
+            {
+                //Creamos la instancia para nuestra contraseña 
+                string password;
+                password = Console.ReadLine();
+                //Declaramos nuestra contraseña de seguridad como "contraseña"
+                if (password == "contraseña")
+                {
+                    //Creamos la instancia
+                    if (nombre == null)
+                    {
+ 
+                        nombre = new Contraseñas();
+                    }
+                    if (pOpcion == 1)
+                        nombre.ContraseñasPriv();
+                }
+                else
+                {
+                    Console.WriteLine("Acceso Denegado, ingrese con una contraseña valida");
+                }
+
+            }
+
+        }
+        //CLASE PRIVADA PROTEGIDA POR PROXY
+        private class Contraseñas
+        {
+            public void ContraseñasPriv()
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Su codigo de entrada al sistema es:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*  0001");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\t\t|--------------BIENVENIDO AL SISTEMA------------|\n");
+                                       
+            }
+        
+        }
+    }
+}
